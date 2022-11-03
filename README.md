c5540f57f91ca6162153d2ccbc9c563f27e3785d
zilaiye <q342023971@126.com>
2022-11-03 21:32:07 +0800

    添加大小驼峰和下划线转换

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # qy-string-transform-ts
-简易字符串内容转换
+简易字符串内容转换TypeScript
+
+## 如何安装
+
+```
+npm install qy-string-transform-ts
+```
+或者
+```
+yarn qy-string-transform-ts
+```
+
+## 如何使用
+
+将英文转大小驼峰和下划线链接
+
+```
+import { Loader } from "qy-string-transform-ts";
+
+let title = 'Products | Services'
+console.log(Loader.parse_name(title, 0, true))
+// "products_services"
+console.log(Loader.parse_name(title, 1, true))
+// "ProductsServices"
+console.log(Loader.parse_name(title, 1, false))
+// "productsServices"
+```
---
+++ src/loader.ts
@@ -0,0 +1,49 @@
+function capitalize(value: string) {
+    return value.charAt(0).toUpperCase() + value.slice(1)
+}
+
+export class Loader {
+    static parse_name(name: string, parse_type = 0, ucfirst = true) {
+        if (name.indexOf('|') > 0) {
+            let arrs = name.split('|').filter(item => item.trim())
+            arrs = arrs.map((item) => { return item.trim() })
+            name = arrs.join('_')
+        }
+        if (name.indexOf('-') > 0) {
+            let arrs = name.split('-').filter(item => item.trim())
+            arrs = arrs.map((item) => { return item.trim() })
+            name = arrs.join('_')
+        }
+        if (name.indexOf('/') > 0) {
+            let arrs = name.split('/').filter(item => item.trim())
+            arrs = arrs.map((item) => { return item.trim() })
+            name = arrs.join('_')
+        }
+        if (name.indexOf(' ') > 0) {
+            let arrs = name.split(' ').filter(item => item.trim())
+            arrs = arrs.map((item) => { return item.trim() })
+            name = arrs.join('_')
+        }
+        if (!name) {
+            return name
+        }
+        if (parse_type) {
+            if (ucfirst) {
+                return name.split("_").map((x) => { return capitalize(x) }).join("")
+            } else {
+                let src = []
+                let src_list: string[] = name.split("_")
+                for (let value of src_list) {
+                    if (src_list.indexOf(value) == 0) {
+                        src.push(value.toLowerCase())
+                    } else {
+                        src.push(capitalize(value))
+                    }
+                }
+                return src.join("")
+            }
+        } else {
+            return name.toLowerCase().replace(" ", "_")
+        }
+    }
+}
